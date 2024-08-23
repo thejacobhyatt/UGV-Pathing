820348e5a52c20e4771c200278c443709c222049
Jacob Hyatt <31117979+thejacobhyatt@users.noreply.github.com>
2024-08-23 09:17:49 -0400

    Initial Commit

---
+++ exenf_dev.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 11 09:36:15 2024
+
+@author: robert.jane
+"""
+
+# solve in gurobi 
+
+
+##Import Python Distribution Modules
+import os
+import sys
+import math
+import time
+import numpy as np
+from scipy.interpolate import interp1d
+from scipy.optimize import minimize
+import matplotlib.pyplot as plt
+
+#Add System Path
+sys.path.insert(1, 'Code/')
+
+#Import Python Custom Moduels
+from exenf_alog import direction_of_travel
+from exenf_alog import exenf_cost
+    
+#Start Time
+start_time = time.time()
+
+#Initial/Final Point (m)
+p1 = [00.00,00.00,+00.00]
+p2 = [10.00,10.00,-40.00]
+
+#Travel Time (s)
+travel_time = 20
+
+#Platform Name
+platform_name = 'moose'
+
+#Added Mass (kg)
+added_mass = 513
+
+#Wind Velocity (m/s)
+wind_velocity = 0
+
+#Originating Wind Direction (deg)
+wind_direction = 0
+
+#Heading Direction (deg)
+heading = direction_of_travel(p1,p2,math)
+
+#Debug
+debug = False
+
+#Input Parameters
+params = [p1, p2, travel_time, platform_name, added_mass, wind_velocity, wind_direction, heading, debug]
+
+#Input Function Handles
+fcns = [np, minimize, interp1d, math, os]
+
+#Exergy/Energy Cost
+Jcon, Jgen, msg = exenf_cost(params,fcns)
+
+#Execution Time
+print("--- %s seconds ---" % (time.time() - start_time))
